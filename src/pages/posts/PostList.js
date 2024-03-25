effd161f9f4c4f65deeefd621a543b3e240197e4
Emilion <emiljadersten@gmail.com>
2024-03-25 06:40:31 +0100

    Create the 'most popular profiles' feature

--- src/pages/posts/PostList.js
+++ src/pages/posts/PostList.js
@@ -13,6 +13,7 @@ import NoResults from "../../assets/no-results.png";
 import { Form } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../../profiles/PopularProfiles";
 
 function PostList({ message, filter = "" }) {
   const [posts, setPosts] = React.useState({ results: [] });
@@ -88,7 +89,7 @@ function PostList({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   );
--- src/profiles/PopularProfiles.js
+++ src/profiles/PopularProfiles.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import appStyles from '../../App.module.css'
-import { Container } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import appStyles from "../App.module.css";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const PopularProfiles = () => {
-  return (
-    <Container>
-        
-    </Container>
-  )
-}
+  const [profileData, setProfileData] = React.useState({
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
 
-export default PopularProfiles
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+            // Limit the number of displayed profiles to 5
+          `/profiles/?ordering=-follower_count&limit=5`
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <Container className={appStyles.Content}>
+      <h4>Most popular profiles</h4>
+      {popularProfiles.results.map((profile) => (
+        <p key={profile.id}>{profile.owner}</p>
+      ))}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
