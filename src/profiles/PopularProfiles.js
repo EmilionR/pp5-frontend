d11d21f67c0f0fd00b182a15476642e61220d119
Emilion <emiljadersten@gmail.com>
2024-03-25 06:14:33 +0100

    Create a module for popular profiles

---
+++ src/profiles/PopularProfiles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import appStyles from '../../App.module.css'
+import { Container } from 'react-bootstrap'
+
+const PopularProfiles = () => {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default PopularProfiles
