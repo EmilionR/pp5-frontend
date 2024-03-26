59009655f774e37e33637d580d0e4c3c989caba0
Emilion <emiljadersten@gmail.com>
2024-03-26 08:04:25 +0100

    Create redirection hook and redirect signed-in users from auth pages

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,9 +15,11 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import styles from "../../styles/SignInUpForm.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
+  useRedirect("loggedIn");
 
   // State to store the sign-in data
   const [signInData, setSignInData] = useState({
@@ -42,7 +44,7 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
-      history.push("/");
+      history.goBack();
     } catch (err) {
       setErrors(err.response?.data);
     }
